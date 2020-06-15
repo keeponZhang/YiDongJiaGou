ddef48dc461ce6eb9b7cfdd99b76f0e3770db128
zhangwengao <zhangwengao@yy.com>
2020-06-15 23:27:04 +0800

    no message

--- 2018/ALiPayPlugin/app/src/main/java/com/dongnao/alvin/alipayplugin/PluginManager.java
+++ 2018/ALiPayPlugin/app/src/main/java/com/dongnao/alvin/alipayplugin/PluginManager.java
@@ -1,6 +1,9 @@
 package com.dongnao.alvin.alipayplugin;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.IntentFilter;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
@@ -8,7 +11,9 @@ import android.content.res.Resources;
 import android.widget.Toast;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.List;
 
 import dalvik.system.DexClassLoader;
 
@@ -65,7 +70,57 @@ public class PluginManager {
         }
         Toast.makeText(context, "成功", Toast.LENGTH_LONG).show();
          }
+    private void parseReceivers(Context context, String path) {
 
+//        Package对象
+//        PackageParser pp = new PackageParser();
+
+//        PackageParser.Package  pkg = pp.parsePackage(scanFile, parseFlags);
+
+
+        try {
+
+            Class   packageParserClass = Class.forName("android.content.pm.PackageParser");
+            Method parsePackageMethod = packageParserClass.getDeclaredMethod("parsePackage", File.class, int.class);
+            Object packageParser = packageParserClass.newInstance();
+            Object packageObj=  parsePackageMethod.invoke(packageParser, new File(path), PackageManager.GET_ACTIVITIES);
+
+            Field receiverField=packageObj.getClass().getDeclaredField("receivers");
+//拿到receivers  广播集合    app存在多个广播   集合  List<Activity>  name  ————》 ActivityInfo   className
+            List receivers = (List) receiverField.get(packageObj);
+
+            Class<?> componentClass = Class.forName("android.content.pm.PackageParser$Component");
+            Field intentsField = componentClass.getDeclaredField("intents");
+
+
+            // 调用generateActivityInfo 方法, 把PackageParser.Activity 转换成
+            Class<?> packageParser$ActivityClass = Class.forName("android.content.pm.PackageParser$Activity");
+//            generateActivityInfo方法
+            Class<?> packageUserStateClass = Class.forName("android.content.pm.PackageUserState");
+            Object defaltUserState= packageUserStateClass.newInstance();
+            Method generateReceiverInfo = packageParserClass.getDeclaredMethod("generateActivityInfo",
+                    packageParser$ActivityClass, int.class, packageUserStateClass, int.class);
+            Class<?> userHandler = Class.forName("android.os.UserHandle");
+            Method getCallingUserIdMethod = userHandler.getDeclaredMethod("getCallingUserId");
+            int userId = (int) getCallingUserIdMethod.invoke(null);
+
+            for (Object activity : receivers) {
+                ActivityInfo
+                        info= (ActivityInfo) generateReceiverInfo.invoke(packageParser,  activity,0, defaltUserState, userId);
+                BroadcastReceiver
+                        broadcastReceiver= (BroadcastReceiver) dexClassLoader.loadClass(info.name).newInstance();
+                List<? extends IntentFilter> intents= (List<? extends IntentFilter>) intentsField.get(activity);
+                for (IntentFilter intentFilter : intents) {
+                    context.registerReceiver(broadcastReceiver, intentFilter);
+                }
+            }
+            //generateActivityInfo
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
     public Resources getResources() {
         return resources;
     }
